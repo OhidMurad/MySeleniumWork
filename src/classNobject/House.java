905f6d2f23341ad9baa8a78e7dca9e12a56e0961
OhidMurad <momsismurad@gmail.com>
2018-08-30 07:03:17 -0400

    First commit

---
+++ src/classNobject/House.java
@@ -0,0 +1,15 @@
+package classNobject;
+
+public class House {
+	int HouseNo = 32;
+	String HouseName = " BOA";
+	
+	public void livingRoom() {
+		System.out.println("Horror Movie");
+	}
+	public void displayAddress() {
+		System.out.print(HouseNo);
+        System.out.println(HouseName);
+	}
+
+}
---
+++ src/classNobject/TestHouse.java
@@ -0,0 +1,13 @@
+package classNobject;
+
+public class TestHouse {
+
+	public static void main(String[] args) {
+		
+		House house = new House();
+		house.displayAddress();
+		house.livingRoom();
+		
+	}
+
+}
